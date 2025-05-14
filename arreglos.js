31589c35fb528ea8f3e79d1173d5e5c137e8c65e
aguskramer445 <136341916+aguskramer445@users.noreply.github.com>
2025-05-14 08:26:49 -0300

    Add files via upload

---
+++ arreglos.js
@@ -0,0 +1,23 @@
+//Punto 1
+let colores = ["Rojo", "Azul", "Verde", "Gris", "Negro"]
+console.log("Arreglo inicial:", colores);
+//Punto 2
+console.log("Primer color:", colores[0]);
+console.log("Último color:", colores[colores.length - 1]);
+//Punto 3
+console.log("Cantidad de elementos:", colores.length);
+//Punto 4
+colores.push("Violeta");
+console.log("Agregado al final:", colores);
+//Punto 5
+colores.unshift("Naranja");
+console.log("Agregado al principio:", colores)
+//Punto 6
+colores.pop("Violeta");
+console.log("Eliminacion del ultimo color", colores)
+//Punto 7
+colores.shift("Naranja");
+console.log("Eliminacion del primer color", colores)
+//Punto 8
+
+
